7d0e7aa6d1f75b620062c420a943aee6ed693931
Mihika Chowdarapu <mihikachowdarapu@gmail.com>
2025-12-17 17:24:06 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project',
-    version = '0.0.0',
+    name = 'medical_chatbot',
+    version = '0.1.0',
     author= 'Mihika Chowdarapu',
     author_email='mihikachowdarapu@gmail.com',
     packages = find_packages(),
